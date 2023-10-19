386c146a1408fc54b644d0579f1d0879155bab4d
Fazluh <fk368414@gmail.com>
2023-10-19 15:37:25 +0530

    Add project files.

---
+++ FFileHandling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FFileHandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //WritingFile();
+
+            //  ReadingFile();
+
+            bool ans= File.Exists(@"D:\\filehandling\\file1.txt");
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+
+        private static void ReadingFile()
+        {
+            FileStream fs = new FileStream(@"D:\\filehandling\\file1.txt", FileMode.Open, FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs);
+
+           string data= sr.ReadToEnd();
+            Console.WriteLine(data);
+            sr.Close();
+            sr.Dispose();
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private static void WritingFile()
+        {
+            try
+            {
+                // File.Create(@"D:\filehandling\file1.txt");
+                FileStream fs = new FileStream(@"D:\\filehandling\\file1.txt", FileMode.Open, FileAccess.Write);
+
+                StreamWriter sw = new StreamWriter(fs);
+
+                sw.WriteLine("Hello this is Demo 1");
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ FFileHandling/assigment_19.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace FFileHandling
+{
+    public class assigment_19
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            
+
+            do {
+                Console.WriteLine("Menu \n");
+                Console.WriteLine("1.Create and Write Person Data\n2.Append new Data Into File\n3.Read the data from file\n4.Exit");
+                int ch;
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+
+                        CreateAndWritePersonData();
+                        break;
+                    case 2:
+
+                        AppendNew();
+                        break;
+                    case 3:
+
+                        ReadFile();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.WriteLine("Do you Want to Continue : ");
+                str= Console.ReadLine();
+            } while(str == "yes");
+            // CreateAndWritePersonData();
+            //AppendNew();
+            //  ReadFile();
+
+            Console.ReadLine();
+        }
+
+        private static void ReadFile()
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"D:\\Assigment_19\\person.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+
+                string prsndata = sr.ReadToEnd();
+                Console.WriteLine($"Person Data ={prsndata}");
+                sr.Close();
+                sr.Dispose();
+                fs.Close();
+                fs.Dispose();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void AppendNew()
+        {
+            try
+            {
+                Console.WriteLine("Enter Name");
+                string namestr = Console.ReadLine();
+                Console.WriteLine("Enter Address");
+                string addstr = Console.ReadLine();
+                Console.WriteLine("Enter City");
+                string citystr = Console.ReadLine();
+                Console.WriteLine("Enter Country");
+                string cntstr = Console.ReadLine();
+
+                FileStream fs = new FileStream(@"D:\\Assigment_19\\person.txt", FileMode.Append,FileAccess.Write);
+                StreamWriter st = new StreamWriter(fs);
+                
+
+                st.WriteLine("Name = " + namestr);
+                st.WriteLine("Address = " + addstr);
+                st.WriteLine("City = " + citystr);
+                st.WriteLine("Country = " + cntstr);
+
+                st.Flush();
+                st.Close();
+                st.Dispose();
+                fs.Close();
+                fs.Dispose();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void CreateAndWritePersonData()
+        {
+            try
+            {
+
+                Console.WriteLine("Enter Name");
+                string namestr = Console.ReadLine();
+                Console.WriteLine("Enter Address");
+                string addstr = Console.ReadLine();
+                Console.WriteLine("Enter City");
+                string citystr = Console.ReadLine();
+                Console.WriteLine("Enter Country");
+                string cntstr = Console.ReadLine();
+                Directory.CreateDirectory(@"D:\\Assigment_19");
+                FileStream fs = new FileStream(@"D:\\Assigment_19\\person.txt", FileMode.CreateNew, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine("Name = " + namestr);
+                sr.WriteLine("Address = " + addstr);
+                sr.WriteLine("City : " + citystr);
+                sr.WriteLine("Country : " + cntstr);
+                Console.WriteLine("Data Entered into file");
+                sr.Flush();
+                sr.Close();
+                sr.Dispose();
+                fs.Close();
+                fs.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+        }
+    }
+}
